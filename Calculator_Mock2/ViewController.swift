f4880edaca6bd0c6b078eb35f0bea51cf42ee9a8
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2016-08-23 17:40:56 +0530

    Initial Commit

---
+++ Calculator_Mock2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator_Mock2
+//
+//  Created by Vyshakh Kandamath on 8/23/16.
+//  Copyright © 2016 Vyshakh Kandamath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
